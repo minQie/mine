6b1ce7e63a2915116483ff8a9e9d3d89973c45c4
chest <tinychest@foxmail.com>
2020-01-16 14:55:44 +0800

    新增 jsr303的自定义拓展：自定义字符串正则校验规则封装、创建编辑的校验标识

---
+++ src/main/java/com/wmc/common/verify/Verify.java
@@ -0,0 +1,36 @@
+package com.wmc.common.verify;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * 默认含有非空校验
+ *
+ * 封装校验类型、可指定请求方式进行校验
+ *
+ * @Author 王敏聪
+ * @Date 2019/11/6 11:43
+ */
+@Constraint(validatedBy = CoreValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+public @interface Verify {
+
+    String message() default "change this property's value is useless";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+
+    /** 校验类型 */
+    VerifyType verifyType();
+
+    /** 是否允许为空（null、空串） */
+    boolean allowBlank() default false;
+
+}
