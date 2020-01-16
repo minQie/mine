2e6a9412915118293f9ffc9b4f0905c04e11788e
chest <tinychest@foxmail.com>
2020-01-16 14:54:11 +0800

    补交

---
+++ src/main/java/com/wmc/config/StaticConfig.java
@@ -0,0 +1,17 @@
+package com.wmc.config;
+
+/**
+ * 全局静态常量配置
+ *
+ * @author 王敏聪
+ * @date 2020-01-16 00:11:09
+ */
+public class StaticConfig {
+
+    /** 自定义枚举序列化规则的 键名 */
+    public static final String ENUM_SERIALIZE_KEY_NAME = "key";
+
+    /** 自定义枚举序列化规则的 值名 */
+    public static final String ENUM_SERIALIZE_VALUE_NAME = "value";
+
+}
