678692e632728fb370f47160be6ade3a234dca5f
minQie <1459049487@qq.com>
2020-01-16 14:56:24 +0800

    新增：json层面的instant时间自定义序列化和反序列化规则

---
+++ src/main/java/com/wmc/config/jackson/InstantJsonDeserializer.java
@@ -0,0 +1,40 @@
+package com.wmc.config.jackson;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.wmc.common.util.DateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Instant Deserializer（只支持默认的格式：DateUtils.COMMON_DATE_FORMAT（yyyy-MM-dd HH:mm:ss））
+ *
+ * @author 王敏聪
+ * @date 2020-01-16 11:44:41
+ */
+@Slf4j
+@JsonComponent
+public class InstantJsonDeserializer extends JsonDeserializer<Instant> {
+
+    final private DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DateUtils.DEFAULT_FORMAT).withZone(ZoneId.of("Asia/Shanghai"));
+
+    @Override
+    public Instant deserialize(JsonParser jp, DeserializationContext ctxt) throws IOException {
+        JsonNode node = jp.getCodec().readTree(jp);
+        String dateString = node.asText();
+        try {
+            return formatter.parse(dateString, Instant::from);
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException(dateString + "格式错误位于第" + (e.getErrorIndex()+1) +"字符，请使用正确格式：" + DateUtils.DEFAULT_FORMAT, e);
+        }
+    }
+
+}
